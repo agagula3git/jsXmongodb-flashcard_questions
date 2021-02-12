291add83cc0dd424230ee4adb96c76c586c667e3
Ajdin Gagula <agagula3@etf.unsa.ba>
2021-02-12 23:05:40 +0100

    initial commit

--- app.js
+++ app.js
@@ -7,7 +7,23 @@ const db = require('./db');
 const collection = "flashcards";
 const PORT = process.env.PORT || 3000;
 
-db.connect((err)=>{
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get('/getFlashcards',(req,res)=>{
+    // get all Todo documents within our todo collection
+    // send back to user as json
+    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(documents);
+        } 
+    });
+});
+
+db.connect((err)=>{ 
     if(err){
         console.log('unable to connect to database');
         process.exit(1);
--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 const dbName = "jsXmongodb";
 
-const uri = "mongodb://localhost:27017";
+const uri = 'mongodb://127.0.0.1:27017'; /*db.serverCmdLineOpts()*/
 
 const state = {
     db: null
@@ -12,7 +12,7 @@ const connect = (f)=>{
     if(state.db){
         f()
     }else{
-        MongoClient.connect(uri,{useNewUrlParser: true}, (err, client)=>{
+        MongoClient.connect(uri,{useUnifiedTopology: true}, (err, client)=>{
             if(err){
                 f(err);
             }else{
@@ -32,5 +32,15 @@ const getPrimaryKey = (_id) =>{
 }
 
 module.exports = {connect, getDB, getPrimaryKey};
+
+/*const MongoClient = require('mongodb').MongoClient
+const Server = require('mongodb').Server;
+
+var mongoClient = new MongoClient(new Server('localhost', 27017));
+mongoClient.open(function(err, mongoClient) {
+  var db1 = mongoClient.db("mydb");
+
+  mongoClient.close();
+});*/
 
 
