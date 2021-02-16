f4b84d573574a4308eacb65bf1f858f9516bf3ee
Ajdin Gagula <agagula3@etf.unsa.ba>
2021-02-17 00:02:14 +0100

    initial commit

--- app.js
+++ app.js
@@ -7,21 +7,66 @@ const db = require('./db');
 const collection = "flashcards";
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
 app.get('/getFlashcards',(req,res)=>{
-    // get all Todo documents within our todo collection
-    // send back to user as json
     db.getDB().collection(collection).find({}).toArray((err,documents)=>{
         if(err)
-            console.log(err);
+            console.log(err); 
         else{
             res.json(documents);
         } 
     });
 });
+
+app.post('/',(req,res)=>{
+    var question = req.body.question;
+    var answer = req.body.answer;
+    console.log(question);
+    const data = {
+        "question": question,
+        "answer": answer
+    }
+    db.getDB().collection(collection).insertOne({question: question, answer: answer},(err,collection)=>{
+        if(err){
+            console.log('Error occured while inserting!')
+            throw(err);
+        }else{
+            res.redirect('correct.html')
+        }
+    });
+
+});
+
+app.put('/:id',(req,res)=>{
+    const cardID = req.params.id;
+    // Find Document By ID and Update
+    db.getDB().collection(collection).findOneAndUpdate({_id: db.getPrimaryKey(cardID)}, {$set: {question: req.body.question, answer: req.body.answer}},{returnOriginal: false},(err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.json(result);
+        }
+    })
+});
+
+app.delete('/:id',(req,res)=>{
+    const cardID = req.params.id;
+    // Find Document By ID and delete document from record
+    db.getDB().collection(collection).findOneAndDelete({_id: db.getPrimaryKey(cardId)},(err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.json(result);
+        }
+    })
+})
 
 db.connect((err)=>{ 
     if(err){
